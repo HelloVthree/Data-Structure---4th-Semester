4449a5fba8a026412c7704875c64e2ef22908957
Brandon <168883192+HelloVthree@users.noreply.github.com>
2026-09-20 21:23:29 +0800

    110. Balanced Binary Tree

---
+++ LeetCode/Chapter-9/LeetCode_110.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return helper(root) != -1;
+        
+    }
+    int helper(TreeNode* root){
+        if(root == nullptr) return 0;
+        int left = helper(root->left);
+        if(left == -1) return -1;
+        int right = helper(root->right);
+        if(right == -1) return -1;
+        if(abs(left - right) > 1) return -1;
+        return max(left, right) + 1;
+    }
+};
