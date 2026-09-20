317fd79b3251f8fc4f8a0cd6c33fe0fba97613e7
JBOSS0520 <jordanyoe99@gmail.com>
2026-09-20 23:11:12 +0800

    20. Valid Parentheses

---
+++ LeetCode/Chapter-6/LeetCode_20.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        if(s.empty()) return true;
+        int n = s.length();
+        for(char c : s){
+            if (c == '(' || c == '[' || c == '{') st.push(c);
+            else {
+                if (st.empty()) return false;
+                
+                if (c == ')' && st.top() != '(') return false;
+                if (c == ']' && st.top() != '[') return false;
+                if (c == '}' && st.top() != '{') return false;
+                st.pop();
+            }
+        }
+    return st.empty();
+    }
+};
