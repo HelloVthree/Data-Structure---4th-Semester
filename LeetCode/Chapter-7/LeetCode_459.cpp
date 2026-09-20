d2e08b2226d0d8e3c0df9b5df994dcf7763f4cd4
JBOSS0520 <jordanyoe99@gmail.com>
2026-09-20 23:25:42 +0800

    459. Repeated Substring Pattern
    Implement a solution to check for repeated substring patterns in a string.

---
+++ LeetCode/Chapter-7/LeetCode_459.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+
+        for(int i =1 ; i <= n/2; i++){
+            if(n%i == 0){
+                string sub = s.substr(0, i);
+                string res = "";
+                for(int j =0; j < n/i; j++){
+                    res+= sub;
+                }
+            if(res == s) return true;
+            }
+        }
+    return false;
+    }
+};
