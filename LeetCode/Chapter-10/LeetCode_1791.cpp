f869f584c34cea803c87568dc71e445f48e185ae
Brandon <168883192+HelloVthree@users.noreply.github.com>
2026-09-20 21:59:06 +0800

    Update and rename LeetCode.cpp to LeetCode_1791.cpp

---
+++ LeetCode/Chapter-10/LeetCode_1791.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int x1 = edges[0][0], x2 = edges[0][1];
+        int y1 = edges[1][0], y2 = edges[1][1];
+
+        if(x1 == y2 || x1 == y1) return x1;
+        return x2;
+        
+    }
+};
